f4c9b7067a7b3bb2e0a1d2754aa0b83cbe62e220
rick2716 <89090386+rick2716@users.noreply.github.com>
2023-09-28 00:20:42 -0500

    Advertencia y cambio de recursos

--- Assets/SoundEffect.cs
+++ Assets/SoundEffect.cs
@@ -16,6 +16,9 @@ public class SoundEffect : MonoBehaviour
     public static event Action OnBienHecho;
     public static event Action OnMalHecho;
 
+    public GameObject AdviceCanvas;
+    private int wrongCount = 0;
+
     private void OnEnable()
     {
         OnBienHecho += PlayBienHechoSound;
@@ -31,11 +34,31 @@ public class SoundEffect : MonoBehaviour
     private void PlayBienHechoSound()
     {
         SonidoSnapZone.PlayOneShot(SonidoBueno, VolumenSonidoBueno);
+        wrongCount = 0;
     }
 
     private void PlayMalHechoSound()
     {
         SonidoSnapZone.PlayOneShot(SonidoMalo, VolumenSonidoMalo);
+        wrongCount++; 
+
+        if (wrongCount == 3)
+        {
+            DisplayAdvice();
+            wrongCount = 0; 
+        }
+    }
+
+    private void DisplayAdvice()
+    {
+        AdviceCanvas.SetActive(true);
+        StartCoroutine(DeactivateAdvice());
+    }
+
+    private IEnumerator DeactivateAdvice()
+    {
+        yield return new WaitForSeconds(10);
+        AdviceCanvas.SetActive(false); 
     }
 
     public void RaiseBienHechoEvent()
