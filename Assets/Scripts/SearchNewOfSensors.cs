49720eb955fcac7cce707911e767080a3e4190de
KenshyST <stiven.higuera@uao.edu.co>
2023-11-07 20:30:24 -0500

    Cambios en el CS
    Mejoras de Expresiones regulares

--- Assets/Scripts/SearchNewOfSensors.cs
+++ Assets/Scripts/SearchNewOfSensors.cs
@@ -23,7 +23,7 @@ public class SearchNewOfSensors : MonoBehaviour
 
     IEnumerator FetchDataFromWebsite()   
     {
-        
+        siteToVisit = Random.Range(0, urlSites.Length);
         UnityWebRequest request = UnityWebRequest.Get(urlSites[siteToVisit]);
         yield return request.SendWebRequest();
 
@@ -36,10 +36,13 @@ public class SearchNewOfSensors : MonoBehaviour
             string htmlContent = request.downloadHandler.text;
 
             string titlePattern = @"<title>(.*?)<\/title>";
+            
             Match titleMatch = Regex.Match(htmlContent, titlePattern);
             if(titleMatch.Success)
             {
                 titleText.text = titleMatch.Groups[1].Value;
+                string stringWithoutHtmlTags = Regex.Replace(titleText.text, "<.*?>|[#&$&@]|[\r\n]", string.Empty);
+                titleText.text = stringWithoutHtmlTags;
             }
             // Suponiendo que el párrafo principal está contenido en las primeras etiquetas <p></p>
             string paragraphPattern = @"<p.*?>(.*?)<\/p>";
@@ -47,28 +50,44 @@ public class SearchNewOfSensors : MonoBehaviour
 
             
             string firstParagraph = paragraphMatches[0].Groups[1].Value;
+            string Pwithout1 = Regex.Replace(firstParagraph, "<.*?>|[#&$&@]|[\r\n]", string.Empty);
+            
             string secondParagraph = paragraphMatches[1].Groups[1].Value;
+            string Pwithout2 = Regex.Replace(secondParagraph, "<.*?>|[#&$&@]|[\r\n]", string.Empty);
+
             string thirdParagraph = paragraphMatches[2].Groups[1].Value;
+            string Pwithout3 = Regex.Replace(thirdParagraph, "<.*?>|[#&$&@]|[\r\n]", string.Empty);
+
             string fourthParagraph = paragraphMatches[3].Groups[1].Value;
-            paragraphText.text = firstParagraph + "; " + "\n" +secondParagraph + "; " + "\n" + thirdParagraph + "; " + "\n" + fourthParagraph;
-                
-   
-            // Suponiendo que la imagen principal tiene una etiqueta <img> (esto obtendría la primera imagen)
-            string imgPattern = @"<img.*?src=['""](.*?)['""]";
-            Match imgMatch = Regex.Match(htmlContent, imgPattern);
-            if (imgMatch.Success)
+            string Pwithout4 = Regex.Replace(fourthParagraph, "<.*?>|[#&$&@]|[\r\n]", string.Empty);
+
+            paragraphText.text = Pwithout1 + "\n" + "\n" + Pwithout2 + "\n" + "\n" + Pwithout3 + "\n" + "\n" + Pwithout4;
+
+
+            
+            bool imgTagExists = Regex.IsMatch(htmlContent, "<img");
+
+            if (imgTagExists)
             {
-                string imgUrl = imgMatch.Groups[1].Value;
-
-                // Si la URL de la imagen es relativa, añadir el dominio principal
-                if (!imgUrl.StartsWith("http"))
+                // Suponiendo que la imagen principal tiene una etiqueta <img> (esto obtendría la primera imagen)
+                string imgPattern = @"<img.*?src=['""](.*?)['""]";
+                // Comprueba si existe una etiqueta <img>
+                Match imgMatch = Regex.Match(htmlContent, imgPattern);
+                if (imgMatch.Success)
                 {
-                    string domain = new System.Uri(urlSites[siteToVisit]).GetLeftPart(System.UriPartial.Authority);
-                    imgUrl = domain + imgUrl;
-                }
+                    string imgUrl = imgMatch.Groups[1].Value;
 
-                StartCoroutine(DownloadImage(imgUrl));
+                    // Si la URL de la imagen es relativa, añadir el dominio principal
+                    if (!imgUrl.StartsWith("http"))
+                    {
+                        string domain = new System.Uri(urlSites[siteToVisit]).GetLeftPart(System.UriPartial.Authority);
+                        imgUrl = domain + imgUrl;
+                    }
+
+                    StartCoroutine(DownloadImage(imgUrl));
+                }
             }
+                
         }
     }
 
@@ -90,6 +109,7 @@ public class SearchNewOfSensors : MonoBehaviour
 
     public void OpenWebPage()
     {
+        if(Application.platform != RuntimePlatform.WindowsPlayer || Application.platform != RuntimePlatform.OSXPlayer || Application.platform != RuntimePlatform.LinuxPlayer)
         Application.OpenURL(urlSites[siteToVisit]);
     }
 
