1af70bfdb8429135d564ec11e0d1aaedb99cd0a4
rick2716 <89090386+rick2716@users.noreply.github.com>
2023-11-13 18:30:42 -0500

    Manos Scriptible Object

---
+++ Assets/3D Models/Objects/ManosSO/ManosSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ManosSO")]
+public class ManosSO : ScriptableObject
+{
+    public Mesh[] rightHand;
+    public Mesh[] leftHand;
+}
